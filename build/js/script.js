261343d3b36248b6b8557c8137751d36836e347d
RitaStark <ritastar13@gmail.com>
2021-03-18 19:41:45 +0300

    slider js

--- build/js/script.js
+++ build/js/script.js
@@ -9,12 +9,10 @@ function openSub(evt, subType) {
     tabcontent[i].style.display = "none";
   }
 
-
   tablinks = document.querySelectorAll(".subscriptions__toggle-item");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace("active", "");
   }
-
 
   document.getElementById(subType).style.display = "block";
   evt.currentTarget.className += "active";
@@ -33,17 +31,14 @@ function plusSlides(n) {
 function showSlides(n) {
   var i;
   var slides = document.querySelectorAll(".trainers__slides");
-  // var dots = document.getElementsByClassName("dot");
+  console.log(slides.length);
   if (n > slides.length) { slideIndex = 1 }
   if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+
   }
-  // for (i = 0; i < dots.length; i++) {
-  //     dots[i].className = dots[i].className.replace(" active", "");
-  // }
+
   slides[slideIndex - 1].style.display = "block";
-  // dots[slideIndex - 1].className += " active";
+
 }
-
-
